9b3c0d8147333e7c87a7ff9c01268c3ed582b2ac
Rafhael <janke.irineu@gmail.com>
2024-02-22 21:27:07 -0300

    AJUSTE DE BOTAO DE ENVIAR

--- frontend/src/components/ChatRoomInput.tsx
+++ frontend/src/components/ChatRoomInput.tsx
@@ -3,6 +3,10 @@ import { socket } from '../websocket';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import EmojiPicker from 'emoji-picker-react'; // Importando o EmojiPicker
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+
 
 interface Message {
   room: string;
@@ -34,12 +38,17 @@ export function ChatRoomInput() {
 
   return (
     <div className="max-w-md w-full flex flex-col gap-3" style={{ marginTop: 'auto' }}>
-    <label
+      <label
         htmlFor="message"
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
       >        
       </label>
-      <div className="relative">
+      <div className="relative flex">
+        <button onClick={() => setShowEmojiPicker(prev => !prev)}
+        style={{
+          fontSize: '1.5rem', // Tamanho maior do emoji
+          
+        }}> 😄 </button> 
         <textarea
           id="message"
           rows={1}
@@ -50,24 +59,9 @@ export function ChatRoomInput() {
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Digite sua mensagem aqui..."
         />
-      </div>
-      <div className="flex justify-between items-left" style={{ width: '100%' }}>
-        <button onClick={() => setShowEmojiPicker(prev => !prev)}> 😄 </button>
-        <button onClick={sendMessage} className="flex items-left justiy-center p-2 bg-blue-500 text-white rounded-full transition-colors duration-300 hover:bg-blue-600 focus:outline-none"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M13.293 6.293a1 1 0 0 1 1.414 1.414l-6 6a1 1 0 0 1-1.414 0l-6-6a1 1 0 1 1 1.414-1.414L10 10.586l3.293-3.293a1 1 0 0 1 1.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
+        <button onClick={sendMessage} className="flex items-center">  
+          <FontAwesomeIcon icon={faPaperPlane} size="2x" /> 
+        </button> 
       </div>
       {showEmojiPicker && (
         <EmojiPicker
@@ -78,4 +72,5 @@ export function ChatRoomInput() {
       )}
     </div>
   );
+  
 }
--- frontend/src/pages/ChatRoomPage.tsx
+++ frontend/src/pages/ChatRoomPage.tsx
@@ -2,10 +2,10 @@ import { ChatRoomInput } from '../components/ChatRoomInput';
 import { ChatRoomMessageList } from '../components/ChatRoomMessageList';
 import { Title } from '../components/Title';
 
+
 export function ChatRoomPage() {
   return (
     <div className="h-full flex flex-col justify-center items-center p-5 gap-8" style={{marginTop: 'auto', background: 'blue', border: '2px solid black', height: '100vh', width: '100vw' }}>
-    <Title title="Sala de Chat" />
     <div style={{ 
       border: '2px solid lightblue',
       background: 'lightblue', 
@@ -14,12 +14,22 @@ export function ChatRoomPage() {
       width: '30%', // largura de 30%
       height: '80%', // altura de 80%
       overflowY: 'auto', // Adiciona uma barra de rolagem vertical quando necessário
-      boxShadow: '5px 5px 15px rgba(0, 0, 0, 0.9)' // Adiciona um sombreamento ao redor da div
+      boxShadow: '5px 5px 15px rgba(0, 0, 0, 0.9)', // Adiciona um sombreamento ao redor da div
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center', // Centraliza os itens horizontalmente
     }}>
+      <div style={{
+        width: '100%',
+        height: '2px',
+        background: 'linear-gradient(to right, #3F51B5, #1976D2)', // Barra com uma tonalidade diferente de azul
+      }} />
+      <Title title="Sala de Chat" />
       <ChatRoomMessageList />
       <ChatRoomInput />
     </div>
   </div>
+  
   
   );
 }
